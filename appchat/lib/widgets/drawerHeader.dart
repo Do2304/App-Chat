56a61fed2da2e5b539ec81affb29801b0b197a01
Luli <lulimanucians198@gmail.com>
2025-08-20 15:25:19 +0700

    Refactor :part2 add image avatar and name instead of text hard folow

--- appchat/lib/widgets/drawerHeader.dart
+++ appchat/lib/widgets/drawerHeader.dart
@@ -1,23 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatDrawerHeader extends StatelessWidget {
   const ChatDrawerHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
     return SizedBox(
-      height: 110,
+      height: 120,
       child: DrawerHeader(
         margin: EdgeInsets.zero,
-        decoration: BoxDecoration(color: Colors.blue),
+        padding: EdgeInsets.zero,
+        // decoration: BoxDecoration(color: Colors.blue),
         child: SizedBox.expand(
-          child: Text(
-            "Luli",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8, bottom: 10),
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(user!.photoURL!),
+                  radius: 28,
+                ),
+              ),
+              SizedBox(width: 6),
+              Expanded(
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        user.displayName!,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+
+                    Align(
+                      alignment: Alignment.centerLeft,
+
+                      child: Text(
+                        user.email!,
+                        style: TextStyle(color: Colors.black),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
