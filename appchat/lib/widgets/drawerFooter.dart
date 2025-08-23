6aace9c19a98ef8a68aea5605270c47e52f13b60
Luli <lulimanucians198@gmail.com>
2025-08-23 09:26:06 +0700

    Refactor: separate function handleLogout

--- appchat/lib/widgets/drawerFooter.dart
+++ appchat/lib/widgets/drawerFooter.dart
@@ -6,19 +6,21 @@ import 'package:shared_preferences/shared_preferences.dart';
 class chatDrawerFooter extends StatelessWidget {
   const chatDrawerFooter({super.key});
 
+  void handleLogout(context) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove("token");
+    await prefs.remove("selectedConversationId");
+    await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut();
+    Navigator.pushReplacementNamed(context, "/login");
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text("Logout"),
       leading: const Icon(Icons.logout),
-      onTap: () async {
-        final prefs = await SharedPreferences.getInstance();
-        await prefs.remove("token");
-        await prefs.remove("selectedConversationId");
-        await FirebaseAuth.instance.signOut();
-        await GoogleSignIn().signOut();
-        Navigator.pushReplacementNamed(context, "/login");
-      },
+      onTap: () => handleLogout(context),
     );
   }
 }
