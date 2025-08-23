b8ee64c16677e334c1686fecdf239a5cbde7a63d
Luli <lulimanucians198@gmail.com>
2025-08-23 10:35:13 +0700

    Feat: add model to display when logout

--- appchat/lib/widgets/drawerFooter.dart
+++ appchat/lib/widgets/drawerFooter.dart
@@ -7,12 +7,39 @@ class chatDrawerFooter extends StatelessWidget {
   const chatDrawerFooter({super.key});
 
   void handleLogout(context) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.remove("token");
-    await prefs.remove("selectedConversationId");
-    await FirebaseAuth.instance.signOut();
-    await GoogleSignIn().signOut();
-    Navigator.pushReplacementNamed(context, "/login");
+    final confirmLogout = await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          actionsAlignment: MainAxisAlignment.center,
+          title: Text("Confirm logout?"),
+          content: Text("Are you sure you want to logout?"),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context, true);
+              },
+              child: Text("Yes"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context, false);
+              },
+              child: Text("No"),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (confirmLogout) {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.remove("token");
+      await prefs.remove("selectedConversationId");
+      await FirebaseAuth.instance.signOut();
+      await GoogleSignIn().signOut();
+      Navigator.pushReplacementNamed(context, "/login");
+    }
   }
 
   @override
