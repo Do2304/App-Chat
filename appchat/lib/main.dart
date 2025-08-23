293202ae362ff5d1e7732392febaa1e5ad6b11ed
Luli <lulimanucians198@gmail.com>
2025-08-23 16:22:58 +0700

    Feat: handle auto login

--- appchat/lib/main.dart
+++ appchat/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'ChatScreen.dart';
 import 'pages/loginPage.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -6,17 +7,22 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+
+  final prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString("token");
+  print("------$token");
+  runApp(MyApp(initialRoute: token == null ? '/login' : '/chat'));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.initialRoute});
+  final String initialRoute;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // home: ChatScreen(),
-      initialRoute: '/login',
+      initialRoute: initialRoute,
       routes: {
         '/login': (context) => const LoginPage(),
         '/chat': (context) => ChatScreen(conversationId: ""),
