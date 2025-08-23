c15f97b7839d7c30d4bd16381e643a2fc82567b4
Luli <lulimanucians198@gmail.com>
2025-08-23 09:14:43 +0700

    Refactor: separate drawerFooter

--- appchat/lib/widgets/appDrawer.dart
+++ appchat/lib/widgets/appDrawer.dart
@@ -1,10 +1,8 @@
+import '/widgets/drawerFooter.dart';
 import '/widgets/chatNew.dart';
 import '/widgets/drawerBody.dart';
 import '/widgets/drawerHeader.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class Appdrawer extends StatelessWidget {
   final VoidCallback startNewConversation;
@@ -20,18 +18,7 @@ class Appdrawer extends StatelessWidget {
           Divider(height: 0),
           ChatDrawerBody(),
           Divider(color: Colors.black, thickness: 1),
-          ListTile(
-            title: Text("Logout"),
-            leading: const Icon(Icons.logout),
-            onTap: () async {
-              final prefs = await SharedPreferences.getInstance();
-              await prefs.remove("token");
-              await prefs.remove("selectedConversationId");
-              await FirebaseAuth.instance.signOut();
-              await GoogleSignIn().signOut();
-              Navigator.pushReplacementNamed(context, "/login");
-            },
-          ),
+          const chatDrawerFooter(),
         ],
       ),
     );
---
+++ appchat/lib/widgets/drawerFooter.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class chatDrawerFooter extends StatelessWidget {
+  const chatDrawerFooter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text("Logout"),
+      leading: const Icon(Icons.logout),
+      onTap: () async {
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.remove("token");
+        await prefs.remove("selectedConversationId");
+        await FirebaseAuth.instance.signOut();
+        await GoogleSignIn().signOut();
+        Navigator.pushReplacementNamed(context, "/login");
+      },
+    );
+  }
+}
