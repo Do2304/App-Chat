bd90b66cf01b14e86018453b5c788963ff48e945
Luli <lulimanucians198@gmail.com>
2025-08-26 08:26:35 +0700

    Refactor: extract chatAppBar widget into a separate file

--- appchat/lib/pages/ChatScreen.dart
+++ appchat/lib/pages/ChatScreen.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import '/widgets/chatAppBody.dart';
 import '/widgets/chatAppBar.dart';
 import '/utils/storageService.dart';
 import '/api/chatApi.dart';
@@ -104,55 +105,11 @@ class _ChatScreenState extends State<ChatScreen> {
         selectedModel: selectedModel,
         onChanged: (val) => setState(() => selectedModel = val!),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              reverse: true,
-              controller: scrollController,
-              itemCount: messages.length,
-              itemBuilder: (context, index) {
-                final msg = messages[index];
-                return Container(
-                  alignment: msg.isSender
-                      ? Alignment.centerRight
-                      : Alignment.centerLeft,
-                  padding: const EdgeInsets.all(8),
-                  child: Container(
-                    padding: const EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                      color: msg.isSender
-                          ? Colors.blue.shade100
-                          : Colors.grey.shade200,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Text(msg.msg),
-                  ),
-                );
-              },
-            ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: TextField(
-                    controller: inputController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Enter message...",
-                    ),
-                  ),
-                ),
-              ),
-              IconButton(
-                icon: const Icon(Icons.send, color: Colors.blue),
-                onPressed: sendMsg,
-              ),
-            ],
-          ),
-        ],
+      body: ChatAppBody(
+        messages: messages,
+        scrollController: scrollController,
+        inputController: inputController,
+        onSend: sendMsg,
       ),
       drawer: Appdrawer(startNewConversation: startNewConversation),
     );
